f1bab14f8b1dd189a0682c494dc507911ffc2253
corkesw <home@stephencorke.com>
2021-10-07 22:05:50 +0100

    update App with compnents

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Header from "./Header";
+import Adder from "./Adder";
+import Lister from "./Lister";
+import { useState } from "react";
 
+// TV and Film watchlist
 function App() {
+  const [list, SetList] = useState({name:"Bergerac", type:"tv"})
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />  
+      <Adder />
+      <Lister list={list}/>
     </div>
   );
 }
